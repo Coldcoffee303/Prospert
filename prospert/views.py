8509956b758bde06b2e572d62b9b3b91b1632571
coldcoffee303 <sharathkodidala7@gmail.com>
2023-04-16 20:11:37 +0530

    [Build v1.0.5] fix bugs and add html, css

--- prospert/views.py
+++ prospert/views.py
@@ -32,7 +32,7 @@ def HomePage(request):
 
 @login_required(login_url='Login')
 def feedPage(request):
-	apikey = os.environ.get("NEWSAPI")
+	apikey = os.environ.get("SECRET")
 	print(type(apikey))
 	feedApi = NewsApiClient(apikey)
 	headlines = feedApi.get_top_headlines(category='business',country='in')
